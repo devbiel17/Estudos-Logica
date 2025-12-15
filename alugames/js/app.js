134f2cfbd208617ee33d72fb9b7e45c92297e7d6
Gabriel Pires <gabrielpiresosorio@gmail.com>
2025-12-15 19:06:27 -0300

    Melhorando alugeul de games

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,3 +1,9 @@
+let jogosAlugados = 0
+
+function imprimirJogosAlugados(){
+    console.log(`Numero de Jogos Alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`); //qual foi o jogo pelo id
     let imagem = gameClicado.querySelector('.dashboard__item__img'); //pega a imagem do jogo (o . seleciona a classe)
@@ -5,15 +11,25 @@ function alterarStatus(id){
 
     
     if(imagem.classList.contains('dashboard__item__img--rented')){  // se na classe contém (...)
+        alert('Clique em "OK" para confirmar');
         imagem.classList.remove('dashboard__item__img--rented');  //remova
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar'; //troca o texto
+        jogosAlugados--;
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
-        botao.classList.add('dashboard__item__button--return')
+        botao.classList.add('dashboard__item__button--return');
+        jogosAlugados++;
     }
+    imprimirJogosAlugados();
 }
+
+//inicia a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function(){
+    jogosAlugados = document.querySelectorAll ('.dashboard__item__img--rented').length;
+    imprimirJogosAlugados();
+})
 
 
 
